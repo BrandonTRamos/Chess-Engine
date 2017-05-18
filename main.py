f6baaeefed68c9930d512df47cbcf490af76a441
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-17 21:28:06 -0400

    initiates evaluate class now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from board import Board
+from evaluate import Evaluate
 myboard=Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 myboard.printBoard()
-
+eval=Evaluate()
+print('\nEvaluation:',eval.evaluate_node(myboard.board))
