e0faf4c97d572c9859bcd2f0923a860aa0cb6e5b
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-17 21:26:50 -0400

    added a basic eval fuction which counts the pieces * piece value and accounts for the pieces position in the piece square table

--- evaluate.py
+++ evaluate.py
@@ -9,6 +9,7 @@ class Evaluate():
                   81, 82, 83, 84, 85, 86, 87, 88,
                   91, 92, 93, 94, 95, 96, 97, 98]
 
+
     whitepawns=[ 0,  0,  0,  0,  0,  0,  0,  0,
                  50, 50, 50, 50, 50, 50, 50, 50,
                  10, 10, 20, 30, 30, 20, 10, 10,
@@ -54,14 +55,14 @@ class Evaluate():
                 -10,  5,  0,  0,  0,  0,  5,-10,
                 -20,-10,-10,-10,-10,-10,-10,-20,]
 
-    blackbishops=[[-20, -10, -10, -10, -10, -10, -10, -20,
+    blackbishops=[-20, -10, -10, -10, -10, -10, -10, -20,
                    -10, 5, 0, 0, 0, 0, 5, -10,
                    -10, 10, 10, 10, 10, 10, 10, -10,
                    -10, 0, 10, 10, 10, 10, 0, -10,
                    -10, 5, 5, 10, 10, 5, 5, -10,
                    -10, 0, 5, 10, 10, 5, 0, -10,
                    -10, 0, 0, 0, 0, 0, 0, -10,
-                   -20, -10, -10, -10, -10, -10, -10,-20]]
+                   -20, -10, -10, -10, -10, -10, -10,-20]
 
     whiterooks=[  0,  0,  0,  0,  0,  0,  0,  0,
                   5, 10, 10, 10, 10, 10, 10,  5,
@@ -134,3 +135,53 @@ class Evaluate():
                        -30, -10, 20, 30, 30, 20, -10, -30,
                        -30, -20, -10, 0, 0, -10, -20, -30,
                        -50, -40, -30, -20, -20, -30, -40, -50]
+
+    def __init__(self):
+        self.onetwentyto64map=self.init_map()
+
+    def init_map(self):
+        onetwentyto64map={}
+        i=0
+        for square in self.legalsquares:
+            onetwentyto64map[square]=i
+            i+=1
+        return onetwentyto64map
+
+    def evaluate_node(self,board):
+        whitescore=0
+        blackscore=0
+        totalscore=0
+        for square in self.legalsquares:
+            if board[square]==1:
+                blackscore+=100
+                blackscore+=self.blackpawns[self.onetwentyto64map[square]]
+            elif board[square]==2:
+                blackscore+=300
+                blackscore += self.blackknights[self.onetwentyto64map[square]]
+            elif board[square]==3:
+                blackscore+=310
+                blackscore += self.blackbishops[self.onetwentyto64map[square]]
+            elif board[square]==4:
+                blackscore+=500
+                blackscore += self.blackrooks[self.onetwentyto64map[square]]
+            elif board[square]==5:
+                blackscore+=900
+                blackscore += self.blackqueen[self.onetwentyto64map[square]]
+            elif board[square]==7:
+                whitescore+=100
+                whitescore += self.whitepawns[self.onetwentyto64map[square]]
+            elif board[square]==8:
+                whitescore+=300
+                whitescore += self.whiteknights[self.onetwentyto64map[square]]
+            elif board[square]==9:
+                whitescore+=310
+                whitescore += self.whitebishops[self.onetwentyto64map[square]]
+            elif board[square]==10:
+                whitescore+=500
+                whitescore += self.whiterooks[self.onetwentyto64map[square]]
+            elif board[square]==11:
+                whitescore+=900
+                whitescore += self.whitequeen[self.onetwentyto64map[square]]
+        totalscore=whitescore-blackscore
+
+        return totalscore/100
