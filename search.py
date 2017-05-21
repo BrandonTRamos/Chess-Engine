697e2ce536f010fddf3c9002a5c93e4fe46f76e0
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-20 20:30:30 -0400

    began a search class

---
+++ search.py
@@ -0,0 +1,11 @@
+class Search():
+
+
+
+    def generateMoves(self,board):
+        movelist = []
+        if board.sideToMove==0:
+            for sq in board.legalsquares:
+                pass
+        return movelist
+
