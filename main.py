445abd19f59ff6152f6e973adeec5e73db0ca2be
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-20 20:32:38 -0400

    time test for 1 million eval function calls = 33 seconds, not able to JIT

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from board import Board
 from evaluate import Evaluate
+from search import Search
+import datetime
 myboard=Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 myboard.printBoard()
 eval=Evaluate()
-print('\nEvaluation:',eval.evaluate_node(myboard.board))
+# search=Search()
+start=datetime.datetime.now()
+for i in range(1000000):
+    eval.evaluate_node(myboard.board)
+end=datetime.datetime.now()
+print("Done",end-start)
+
