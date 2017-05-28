a9fdc8c0d03e2b82dfab274fb8e535b56ac0ed6d
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-27 22:34:52 -0400

    time test for 1 million eval calls:~ 7 seconds

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ from board import Board
 from evaluate import Evaluate
 from search import Search
 import datetime
+
 myboard=Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 myboard.printBoard()
 eval=Evaluate()
-# search=Search()
-start=datetime.datetime.now()
-for i in range(1000000):
-    eval.evaluate_node(myboard.board)
-end=datetime.datetime.now()
-print("Done",end-start)
+search=Search()
+
+whiteknightmoves=search.generateKnightmoves(myboard,myboard.White)
+blackknightmoves=search.generateKnightmoves(myboard,myboard.Black)
+
+print("white knight moves:",whiteknightmoves)
+for move in whiteknightmoves:
+    print("from:",(move>>7)&127)
+    print("to:", move & 127)
+print("\nblack knight moves:",blackknightmoves)
+for move in blackknightmoves:
+    print("from:",(move>>7)&127)
+    print("to:", move & 127)
+
+
+
+# print("\nbegin eval...")
+# start=datetime.datetime.now()
+# for i in range(1):
+#     eval.evaluate_node(myboard)
+# end=datetime.datetime.now()
+# print('finish eval...Time:',end-start)
 
