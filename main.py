eefec23418c1e2f2c08d130d56f428267ca4b966
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-28 22:01:03 -0400

    Validation needs to be done on recursive search function

--- main.py
+++ main.py
@@ -3,21 +3,14 @@ from evaluate import Evaluate
 from search import Search
 import datetime
 
-myboard=Board('rnbqkbnr/pppppppp/8/8/8/7r/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-myboard.printBoard()
+myboard=Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+# myboard.printBoard()
 eval=Evaluate()
 search=Search()
+myboard.printBoard()
+print('\nWhite')
+search.searchPosition(myboard,eval,myboard.White,3)
+print('\n Final Board:')
+myboard.printBoard()
 
-whiteknightmoves=search.generateKnightmoves(myboard,myboard.White)
-myboard.makeMove(whiteknightmoves)
-blackknightmoves=search.generateKnightmoves(myboard,myboard.Black)
-
-
-
-# print("\nbegin eval...")
-# start=datetime.datetime.now()
-# for i in range(1):
-#     eval.evaluate_node(myboard)
-# end=datetime.datetime.now()
-# print('finish eval...Time:',end-start)
 
