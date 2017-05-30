4f56f8db922d199ee3740ab6d46ec27e0db0f589
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-30 19:50:56 -0400

    implemented minimax search, still needs move generation and best line result tested

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 class Search():
-
+    nodes=0
     def generateKnightmoves(self,board,side):
         knightoffsets=[8,12,19,21,-8,-12,-19,-21]
         moves=[]
@@ -10,7 +10,7 @@ class Search():
                     capturedPiece = 0
                     move = 8
                     toSquare = fromsquare + offset
-                    if board.board[toSquare]!=99 and board.board[toSquare]!=6  and toSquare not in board.whitepiecesq:
+                    if board.board[toSquare]!=board.OffBoard and board.board[toSquare]!=board.BlackKing  and toSquare not in board.whitepiecesq:
                         if board.board[toSquare] in board.blackPieces:
                             capturedPiece=board.board[toSquare]
                         move=(move<<7)|fromsquare
@@ -24,7 +24,7 @@ class Search():
                     capturedPiece = 0
                     move = 2
                     toSquare=fromsquare+offset
-                    if board.board[toSquare]!=99 and board.board[toSquare]!=12 and toSquare not in board.blackpiecesq:
+                    if board.board[toSquare]!=board.OffBoard and board.board[toSquare]!=board.WhiteKing and toSquare not in board.blackpiecesq:
                         if board.board[toSquare] in board.whitePieces:
                             capturedPiece=board.board[toSquare]
                         move=(move<<7)|fromsquare
@@ -39,33 +39,62 @@ class Search():
 
 
 
+    def get_max(self,line,bestline):
+        if line[0]>bestline[0]:
+            return line
+        else:
+            return bestline
+    def get_min(self,line,bestline):
+        if line[0]<bestline[0]:
+            return line
+        else:
+            return bestline
 
 
-
-    def searchPosition(self,board,evaluate,side,depth):
+    def mini_max(self,board,evaluate,side,depth,movelist):
         board.sideToMove=side
-        print('side to move',board.sideToMove)
+        print('____________________________________________________')
+        print('\nside to move',board.sideToMove)
         if depth==0:
+            moves=[]
+            for i in movelist:
+                moves+=[i]
             print('Max depth reached, evaluation:',evaluate.evaluate_node(board))
+            print('Result:',(evaluate.evaluate_node(board),moves))
+            print('________________________________________________')
+            self.nodes+=1
+            return (evaluate.evaluate_node(board),moves)
 
         elif side==board.White:
+            bestLine=(-1000,[])
             whitemoves=self.generateKnightmoves(board,board.White)
             for move in whitemoves:
-                print('whitemoves @ depth',depth,':',whitemoves)
-                print('white making @ depth:',depth,'Move:',move)
+                movelist.append(move)
+                # print('whitemoves @ depth',depth,':',whitemoves)
+                # print('white making @ depth:',depth,'Move:',move)
                 board.makeMove(move)
                 board.printBoard()
-                self.searchPosition(board,evaluate,board.Black,depth-1)
-                print('whiteunmaking @ depth:',depth,'Move:',move)
+                line=self.mini_max(board,evaluate,board.Black,depth-1,movelist)
+                # print('whiteunmaking @ depth:',depth,'Move:',move)
+                bestLine = self.get_max(line,bestLine)
                 board.unmakeMove(move)
+                movelist.remove(move)
+            return bestLine
+
 
         else:
+            bestLine=(1000,[])
             blackmoves=self.generateKnightmoves(board,board.Black)
             for move in blackmoves:
-                print('blackmoves:',blackmoves)
-                print('black making @ depth:', depth,'Move:',move)
+                movelist.append(move)
+                # print('blackmoves:',blackmoves)
+                # print('black making @ depth:', depth,'Move:',move)
                 board.makeMove(move)
                 board.printBoard()
-                self.searchPosition(board,evaluate,board.White,depth-1)
-                print('black unmaking @ depth:', depth,'Move:',move)
+                line=self.mini_max(board,evaluate,board.White,depth-1,movelist)
+                # print('black unmaking @ depth:', depth,'Move:',move)
+                bestLine = self.get_min(line,bestLine)
                 board.unmakeMove(move)
+                movelist.remove(move)
+            return bestLine
+
