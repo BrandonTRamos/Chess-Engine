6ed9227001a27f1067462e9385255f0ff569aa14
Brandon Ramos <BrandonTRamos@gmail.com>
2017-05-30 19:50:00 -0400

    .

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from search import Search
 import datetime
 
 myboard=Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-# myboard.printBoard()
 eval=Evaluate()
 search=Search()
-myboard.printBoard()
-print('\nWhite')
-search.searchPosition(myboard,eval,myboard.White,3)
-print('\n Final Board:')
-myboard.printBoard()
-
+# myboard.printBoard()
+bestline=search.mini_max(myboard,eval,myboard.White,3,[])
+print('--------------------------------------------------')
+print('Evaluation:',bestline[0],'Bestline:',bestline[1])
+# print('\n Final Board:')
+# myboard.printBoard()
+print('Nodes evaluated:',search.nodes)
 
