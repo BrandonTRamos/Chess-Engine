c64ba7044c50aae9ba4c5ee9bda04835a126801c
Brandon Ramos <BrandonTRamos@gmail.com>
2017-06-19 23:02:37 -0400

    .

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from evaluate import Evaluate
 from search import Search
 import datetime
 
-myboard=Board('7k/8/8/4KN1r/8/8/8/8 w KQkq - 0 1')
+myboard=Board('7k/2b5/3N4/4K3/8/8/8/8 w KQkq - 0 1')
 eval=Evaluate()
 search=Search()
 myboard.printBoard()
-print(search.is_pinned(myboard,56))
+print(search.is_pinned(myboard,44))
 # bestline=search.mini_max(myboard,eval,myboard.sideToMove,7,[],-1000,1000)
 # print('--------------------------------------------------')
 # print('Evaluation:',bestline[0],'Bestline:',search.moves_toString(bestline[1]))
