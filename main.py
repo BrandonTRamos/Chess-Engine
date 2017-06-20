3073120ec7b01c0f0e8b37f83b67d6d3374dd8a7
Brandon Ramos <BrandonTRamos@gmail.com>
2017-06-19 22:12:26 -0400

    .

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from evaluate import Evaluate
 from search import Search
 import datetime
 
-myboard=Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+myboard=Board('1Q5K/8/3n4/4k3/8/8/8/8 b KQkq - 0 1')
 eval=Evaluate()
 search=Search()
-# myboard.printBoard()
-bestline=search.mini_max(myboard,eval,myboard.sideToMove,4,[],-1000,1000)
-print('--------------------------------------------------')
-print('Evaluation:',bestline[0],'Bestline:',search.moves_toString(bestline[1]))
-# print('\n Final Board:')
-# myboard.printBoard()
-print('Nodes evaluated:',search.nodes)
+myboard.printBoard()
+print(search.is_pinned(myboard,44))
+# bestline=search.mini_max(myboard,eval,myboard.sideToMove,7,[],-1000,1000)
+# print('--------------------------------------------------')
+# print('Evaluation:',bestline[0],'Bestline:',search.moves_toString(bestline[1]))
+# # print('\n Final Board:')
+# # myboard.printBoard()
+# print('Nodes evaluated:',search.nodes)
+
 
