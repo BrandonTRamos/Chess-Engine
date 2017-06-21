4315aa8a5070fde8ecb78a69e18c3384a9f9d98b
Brandon Ramos <BrandonTRamos@gmail.com>
2017-06-21 18:43:34 -0400

    added a function to check if a square is pinned. needs further validaton

--- search.py
+++ search.py
@@ -57,43 +57,46 @@ class Search():
                     return False
 
     def generateKnightmoves(self,board,side):
-        knightoffsets=[8,12,19,21,-8,-12,-19,-21]
+        knightoffsets=[-8,-12,-19,-21,8,12,19,21]
         moves=[]
         if side==board.White:
             for square in board.whiteKnightsq:
-                fromsquare=square
-                for offset in knightoffsets:
-                    capturedPiece = 0
-                    move = 8
-                    toSquare = fromsquare + offset
-                    if board.board[toSquare]!=board.OffBoard and board.board[toSquare]!=board.BlackKing and toSquare not in board.whitepiecesq:
-                        if board.board[toSquare] in board.blackPieces:
-                            capturedPiece=board.board[toSquare]
-                        move=(move<<7)|fromsquare
-                        move=(move<<7)|toSquare
-                        move=(move<<4)|capturedPiece
-                        moves.append(move)
+                if self.is_pinned(board,square):
+                    pass
+                else:
+                    fromsquare=square
+                    for offset in knightoffsets:
+                        capturedPiece = 0
+                        move = 8
+                        toSquare = fromsquare + offset
+                        if board.board[toSquare]!=board.OffBoard and board.board[toSquare]!=board.BlackKing and toSquare not in board.whitepiecesq:
+                            if board.board[toSquare] in board.blackPieces:
+                                capturedPiece=board.board[toSquare]
+                            move=(move<<7)|fromsquare
+                            move=(move<<7)|toSquare
+                            move=(move<<4)|capturedPiece
+                            moves.append(move)
+            return moves
         else:
+            knightoffsets = [8, 12, 19, 21, -8, -12, -19, -21]
             for square in board.blackKnightsq:
-                fromsquare=square
-                for offset in knightoffsets:
-                    capturedPiece = 0
-                    move = 2
-                    toSquare=fromsquare+offset
-                    if board.board[toSquare]!=board.OffBoard and board.board[toSquare]!=board.WhiteKing and toSquare not in board.blackpiecesq:
-                        if board.board[toSquare] in board.whitePieces:
-                            capturedPiece=board.board[toSquare]
-                        move=(move<<7)|fromsquare
-                        move=(move<<7)|toSquare
-                        move=(move<<4)|capturedPiece
-                        moves.append(move)
+                if self.is_pinned(board,square):
+                    pass
+                else:
+                    fromsquare=square
+                    for offset in knightoffsets:
+                        capturedPiece = 0
+                        move = 2
+                        toSquare=fromsquare+offset
+                        if board.board[toSquare]!=board.OffBoard and board.board[toSquare]!=board.WhiteKing and toSquare not in board.blackpiecesq:
+                            if board.board[toSquare] in board.whitePieces:
+                                capturedPiece=board.board[toSquare]
+                            move=(move<<7)|fromsquare
+                            move=(move<<7)|toSquare
+                            move=(move<<4)|capturedPiece
+                            moves.append(move)
 
-        # print('BOARD WITHIN GENMOVES:')
-        # board.printBoard()
-        # print('MOVES WITHIN GENMOVES:',moves)
-        return moves
-
-
+            return moves
 
     def get_max(self,line,bestline):
         if line[0]>bestline[0]:
@@ -110,13 +113,13 @@ class Search():
 
     def mini_max(self,board,evaluate,side,depth,movelist,alpha,beta):
         board.sideToMove=side
-        print('____________________________________________________')
-        print('\nside to move',board.sideToMove)
+        # print('____________________________________________________')
+        # print('\nside to move',board.sideToMove)
         if depth==0:
             moves=movelist[:]
-            print('Max depth reached, evaluation:',evaluate.evaluate_node(board))
-            print('Result:',(evaluate.evaluate_node(board),moves))
-            print('________________________________________________')
+            # print('Max depth reached, evaluation:',evaluate.evaluate_node(board))
+            # print('Result:',(evaluate.evaluate_node(board),moves))
+            # print('________________________________________________')
             self.nodes+=1
             return (evaluate.evaluate_node(board),moves)
 
@@ -128,7 +131,7 @@ class Search():
                 # print('whitemoves @ depth',depth,':',whitemoves)
                 # print('white making @ depth:',depth,'Move:',move)
                 board.makeMove(move)
-                board.printBoard()
+                # board.printBoard()
                 line=self.mini_max(board,evaluate,board.Black,depth-1,movelist,alpha,beta)
                 # print('whiteunmaking @ depth:',depth,'Move:',move)
                 maxbestLine = self.get_max(line,maxbestLine)
@@ -148,7 +151,7 @@ class Search():
                 # print('blackmoves:',blackmoves)
                 # print('black making @ depth:', depth,'Move:',move)
                 board.makeMove(move)
-                board.printBoard()
+                # board.printBoard()
                 line=self.mini_max(board,evaluate,board.White,depth-1,movelist,alpha,beta)
                 # print('black unmaking @ depth:', depth,'Move:',move)
                 minbestLine = self.get_min(line,minbestLine)
